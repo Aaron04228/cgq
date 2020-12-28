063f2968a323dbbedc481fa558e1f99a05cb4d8b
Aaron04228 <74747364+Aaron04228@users.noreply.github.com>
2020-12-28 16:13:24 +0800

    Update ValueAnimatorUtil.java

--- utils/ValueAnimatorUtil.java
+++ utils/ValueAnimatorUtil.java
@@ -5,13 +5,6 @@ import android.support.annotation.NonNull;
 
 import java.lang.reflect.Field;
 
-/**
- * Created by wuyr on 17-11-11 下午11:59.
- */
-
-/**
- * 用来重置动画缩放时长的工具类
- */
 public class ValueAnimatorUtil {
 
     /**
